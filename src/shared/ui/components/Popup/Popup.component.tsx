f132a24e36f225c98a1bf3670c292027f7bcfee4
iFB24M <fakem1ner@yandex.ru>
2024-03-16 16:14:26 +0400

    start migrating to fsd: move pages to pages move ui lib to shared move container to ui lib (idk) move registration actions to hooks (rewrite all pages)
    additional:
    remove storybook and .stories files
    replace classList module to clsx module
    
    move global states to cookies
    remove package-lock file to remove error on deploying

--- src/shared/ui/components/Popup/Popup.component.tsx
+++ src/shared/ui/components/Popup/Popup.component.tsx
@@ -1,9 +1,9 @@
-import { classList } from '../../../functions/classList'
+import clsx from 'clsx'
 import styles from './Popup.module.scss'
 import { PopupProps } from './Popup.props'
 
 export const Popup = (props: PopupProps) => {
-	return <div {...classList(styles.popup, styles[props.opened ? 'opened' : ''])}>
+	return <div className={clsx(styles.popup, props.opened && styles.opened)}>
 		<div className={styles.window}>
 			{props.children}
 		</div>
